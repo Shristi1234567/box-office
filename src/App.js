7086396645a603b043fad65ad3280d37ebca37b8
shristi <shristi1234567>
2021-04-08 17:06:32 +0530

    created react page

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import {Switch,Route} from 'react-router-dom'
+
+function App() {
+  return <Switch>
+      <Route exact path="/">
+        This is Home Page
+      </Route>
+
+      <Route exact path="/starred">
+        This is Starred Page
+      </Route>
+
+      <Route>
+        This is 404 Page.
+      </Route>
+    </Switch>;
+
+}
+
+export default App;
