bb87c05c338e0cae3ecc828ab99fb3e95d5d635b
shristi <shristi1234567>
2021-04-09 16:51:54 +0530

    created preview cards for api data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,9 @@
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable arrow-body-style */
 import React,{useState} from 'react'
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout'
+import ShowGrid from '../components/show/ShowGrid';
 import {apiGet} from '../misc/config';
 
 export const Home = () => {
@@ -53,11 +55,16 @@ export const Home = () => {
         }
         if(results && results.length > 0 )
         {
-            return results[0].show ? results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-            )) : results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-            ));
+            return results[0].show ? (
+                <ShowGrid data={results}/>
+            ) : (
+                <ActorGrid data={results}/>                
+            );
+            //return results[0].show ? results.map(item => (
+              //  <div key={item.show.id}>{item.show.name}</div>
+                //)) : results.map(item => (
+                //<div key={item.person.id}>{item.person.name}</div>
+               // ));
         }
         return null;
     }
